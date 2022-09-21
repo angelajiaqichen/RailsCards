8a870f49f49cc522c1b471e042c9f4aaf087168b
Angela Chen <angelajiaqichen@gmail.com>
2022-09-20 23:22:40 -0400

    Initial Commit

---
+++ RailsCards/RailsCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailsCardsApp.swift
+//  RailsCards
+//
+//  Created by Angela Chen on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RailsCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
